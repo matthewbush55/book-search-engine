8cb5912451d2df90b02a8757dd491f1920918417
Matt Bush <59295261+matthewbush55@users.noreply.github.com>
2021-10-06 20:57:16 -0600

    Updated schemas for routing

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String!
+    email: String!
+    password: String
+    bookCount: Int
+    savedBooks: [Book]!
+  }
+
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String!
+    title: String!
+    bookId: String!
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me(userId: ID!): User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    deleteBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
